e215de6ba9a3933eb5f4683d8898b9ce9e5884a6
Witt Phillips <witt.phillips@yale.edu>
2024-03-18 10:24:14 -0400

    reading in

--- analysis.py
+++ analysis.py
@@ -1,21 +1,39 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
 
-path = 'AI On-Campus Research Survey (Responses).xlsx'
-df = pd.read_excel(path, engine='openpyxl')
 
-print(df.head())
+df = pd.read_csv('data.csv')
+og_df = df
+#print(df.head())
 
-# plt.scatter(df.iloc[:, 0], df.iloc[:, 1])
-# plt.show()
+def preprocess(data):
+    #Drop currently unused columns
+    data.drop(['PassengerId', 'Name', 'Ticket'], axis=1, inplace=True)
+
+    # Normalize continuous variables (if they exist)
+    to_normalize = ['Age', 'SibSp', 'Parch', 'Fare', 'PClass']
+    to_normalize = [c for c in to_normalize if c in data.columns]
+    if to_normalize:
+        data[to_normalize] = MinMaxScaler().fit_transform(data[to_normalize])  
+    #print(data[to_normalize].head())
+
+    #woman = 0, man = 1
+    if 'Sex' in data.columns:
+        data['Sex'] = data['Sex'].replace({'female': 0, 'male': 1})
 
 if __name__ == '__main__':
     print("Hello world!")
 
-    # Read input
-    col_input = input("Enter prediction cols by #, separated by space:")
-    active_cols = [df.columns[int(num)] for num in col_input.split()]
-        #print(active_cols)
-    
+    preprocess(df)
+
+    # Read input && create active dataframe (adf) from selected cols
+    col_input = input("Enter prediction cols by #, separated by space: ")
+    idxs = [int(num) for num in col_input.split()]
+    active_cols = [df.columns[idx] for idx in idxs]
+    adf = df.loc[:, active_cols]
+    #print(adf.head())
+
+
     
